89c74b8e7b7109487c0674fed3dee0f4ec35f785
dtecnologiaymas-cpu <dtecnologiaymas@gmail.com>
2026-06-12 12:45:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ setTimeout(function(){
     document.getElementById('splash').style.display='none';
     document.getElementById('app').classList.remove('hidden');
 }, 4000);
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+
+let actual = 0;
+
+function mostrarSlide(index){
+
+    slides.forEach(slide =>
+        slide.classList.remove('active')
+    );
+
+    dots.forEach(dot =>
+        dot.classList.remove('active-dot')
+    );
+
+    slides[index].classList.add('active');
+    dots[index].classList.add('active-dot');
+}
+
+setInterval(() => {
+
+    actual++;
+
+    if(actual >= slides.length){
+        actual = 0;
+    }
+
+    mostrarSlide(actual);
+
+}, 3500);
